9adb782490173ff76a3d5c128208fb0ba7125bba
Bhanupratap Singh <pratapsinghbhanu444@gmail.com>
2023-04-18 15:47:16 +0530

    Inital commit

---
+++ logic.js
@@ -0,0 +1,24 @@
+const performAction = async () => {
+    document.getElementById("agreepolicy").click();
+};
+
+switch (window.location.hostname) {
+  case "http://10.10.0.1/":
+    performAction();
+    break;
+
+
+  case "https://internet.lpu.in/24online/webpages/client.jsp":
+    performAction();
+
+    // document.body.innerHTML = performAction();
+    console.log("working");
+    break;
+
+  case "internet.lpu.in":
+    // document.body.innerHTML = performAction();
+    performAction();
+
+    console.log("working");
+    break;
+}
